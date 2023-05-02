42852c2b16cfc4ead649b8f541a64c7465adf8da
silviano <sylvoon@orange.fr>
2023-05-02 15:05:51 +0200

    Guard routine done

--- Assets/Guard.cs
+++ Assets/Guard.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class Guard : MonoBehaviour
 {
-    int endCP;
-    public List<GameObject> Checkpoints = new List<GameObject>();
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        endCP = Checkpoints.Count;
-    }
+    public List<Transform> points = new List<Transform>(); // La liste de points à atteindre
+    public float speed = 5.0f; // La vitesse de déplacement
+    private int currentPoint = 0; // Le point actuel dans la liste
 
-    // Update is called once per frame
     void Update()
     {
-        //for(i=0, i<Checkpoints.Count, i++)
+        // Si la liste de points est vide, ne rien faire
+        if (points.Count == 0)
+        {
+            return;
+        }
+
+        // Calculer la distance entre l'objet et le point actuel
+        float distance = Vector3.Distance(transform.position, points[currentPoint].position);
+
+        // Si la distance est très petite, passer au point suivant
+        if (distance < 0.1f)
+        {
+            currentPoint++;
+            if (currentPoint >= points.Count)
+            {
+                currentPoint = 0;
+            }
+        }
+
+        // Calculer la direction vers le point actuel
+        Vector3 direction = points[currentPoint].position - transform.position;
+        direction.Normalize();
+
+        // Déplacer l'objet vers le point actuel
+        transform.position += direction * speed * Time.deltaTime;
     }
 }
